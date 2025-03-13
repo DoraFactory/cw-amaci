5f777a0e4df85aada59ae128b9a4a43ce9e30d3a
99Kies <1290017556@qq.com>
2025-03-13 16:32:16 +0800

    update amaci config

--- contracts/amaci/src/contract.rs
+++ contracts/amaci/src/contract.rs
@@ -313,11 +313,11 @@ pub fn instantiate(
                                                      // let deactivate_timeout = Timestamp::from_seconds(15 * 60); // 15 minutes
                                                      // let tally_timeout = Timestamp::from_seconds(1 * 3600); // 1 hour
 
-    let deactivate_timeout = Timestamp::from_seconds(5); // for test
-    let tally_timeout = Timestamp::from_seconds(30); // for test
+    // let deactivate_timeout = Timestamp::from_seconds(5); // for test
+    // let tally_timeout = Timestamp::from_seconds(30); // for test
 
-    // let deactivate_timeout = Timestamp::from_seconds(5 * 60); // 5 minutes
-    // let tally_timeout = Timestamp::from_seconds(30 * 60); // 30 minutes
+    let deactivate_timeout = Timestamp::from_seconds(5 * 60); // 5 minutes
+    let tally_timeout = Timestamp::from_seconds(30 * 60); // 30 minutes
     DEACTIVATE_TIMEOUT.save(deps.storage, &deactivate_timeout)?;
     TALLY_TIMEOUT.save(deps.storage, &tally_timeout)?;
     DELAY_RECORDS.save(deps.storage, &DelayRecords { records: vec![] })?;
